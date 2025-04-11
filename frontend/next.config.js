070322ac9daea73660c454bb8e5a427e6f1ea008
ralfferreira <ralfferreira995@gmail.com>
2025-04-11 17:00:44 -0300

    feat: setup Docker environment on repo, add Dockerfile in frontend

---
+++ frontend/next.config.js
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  output: 'standalone',
+  images: {
+    domains: ['localhost'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  experimental: {
+    serverActions: true,
+  },
+}
+
+module.exports = nextConfig
